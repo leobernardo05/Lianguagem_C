14fe63048d16402cab2884738984621d20f79ee7
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-06-09 21:24:11 -0300

    update

---
+++ Ex_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int a,b,c;
+    printf("Digite suas notas:");
+    scanf("%d %d %d", &a, &b, %c);
+
+    float media = (a + b + c)/3;
+
+    if(media <= 10 || media >= 0){
+    } else {
+
+    }
+    
+    return 0;
+}
--- Hello_word.c
+++ Hello_word.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void){
-    printf('Hello word, danado!')
+    printf('Hello word, danado!\n')
     return 0;
 }
