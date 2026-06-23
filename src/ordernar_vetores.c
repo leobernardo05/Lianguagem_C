ed6322147340297b2f8a51d42bbd293565980bdb
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-06-22 21:03:09 -0300

    Create ordenar_vetores.c

---
+++ src/ordernar_vetores.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// ordem de vetores!!
+int main(void){
+
+    int v[10];
+    
+    for(int i=0;i<10;i++) {
+        printf("Digite 10 valores em ordem crescente:\n");
+        scanf("%d", &v[i]);
+  
+    }
+    
+    return 0;
+}
