8e0eb693ab945f3ee7a89d6792778b8f3da22977
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-06-23 10:51:45 -0300

    update

--- src/ordernar_vetores.c
+++ src/ordernar_vetores.c
@@ -2,13 +2,23 @@
 
 // ordem de vetores!!
 int main(void){
-
+    int maior, temp;
     int v[10];
-    
-    for(int i=0;i<10;i++) {
-        printf("Digite 10 valores em ordem crescente:\n");
-        scanf("%d", &v[i]);
-  
+    // adicionar valores no array
+    for(int i=0; i<10; i++) {
+        printf("Digite 10 valores:\n");
+        scanf("%d", &v[i]);   
+    }
+
+    //----- verificação e ordenação
+    for (int i=0; i<10; i++){
+        for ( i = 0; i < 10; i++){
+            
+        }
+        
+
+
+
     }
     
     return 0;
