d1bc3a1cfafb54e87f89f2480be07a8818fd90b0
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-08-03 20:59:57 -0300

    update

--- src/matrizes/SomaDiferencaMatrizes.c
+++ src/matrizes/SomaDiferencaMatrizes.c
@@ -2,44 +2,74 @@
 
 // exibição de matriz
 int main(void){
-    int matriz[3][3], valor;
+    int matriz1[3][3], matriz2[3][3], soma[3][3], subtracao[3][3];
     int linha = 3;
     int colunas = 3;
     // preenchendo matriz
     for (int i = 0; i < linha; i++){
         for (int j = 0; j < colunas; j++){
             printf("Digite um numero para a linha %d e a colunas %d: ", i, j);
-            scanf("%d", &matriz[i][j]);
+            scanf("%d", &matriz1[i][j]);
         }
         printf("\n");
     }
 
-    // exibição matriz
     for (int i = 0; i < linha; i++){
         for (int j = 0; j < colunas; j++){
-            printf("%d ", matriz[i][j]);
+            printf("Digite um numero para a linha %d e a colunas %d: ", i, j);
+            scanf("%d", &matriz2[i][j]);
         }
         printf("\n");
     }
 
-
-    printf("Digite o escalar:\n");
-    scanf("%d", &valor);
-    // exibição matriz
+    // exibição matriz1
     for (int i = 0; i < linha; i++){
         for (int j = 0; j < colunas; j++){
-            matriz[i][j] = valor * matriz[i][j]; 
+            printf("%d ", matriz1[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    // exibição matriz2
+    for (int i = 0; i < linha; i++){
+        for (int j = 0; j < colunas; j++){
+            printf("%d ", matriz2[i][j]);
         }
         printf("\n");
     }
 
-    // exibição matriz
+    // matriz soma
     for (int i = 0; i < linha; i++){
         for (int j = 0; j < colunas; j++){
-            printf("%d ", matriz[i][j]);
+            soma[i][j] = matriz1[i][j] + matriz2[i][j];
         }
         printf("\n");
     }
+    printf("exibição soma: \n");
+    // matriz soma
+    for (int i = 0; i < linha; i++){
+        for (int j = 0; j < colunas; j++){
+            printf("%d ", soma[i][j]);
+        }
+        printf("\n");
+    }
+
+    // matriz subtração
+    for (int i = 0; i < linha; i++){
+        for (int j = 0; j < colunas; j++){
+            subtracao[i][j] = matriz1[i][j] - matriz2[i][j];
+        }
+        printf("\n");
+    }
+    printf("exibição subtração: \n");
+    // matriz subtração
+    for (int i = 0; i < linha; i++){
+        for (int j = 0; j < colunas; j++){
+            printf("%d ", subtracao[i][j]);
+        }
+        printf("\n");
+    }
+    
 
     
 }
